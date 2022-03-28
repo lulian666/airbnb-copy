472c65b5faae16cde346e19d7e96aa06eaf2a728
lulian666 <644139490@qq.com>
2022-03-28 23:51:17 +0800

    static-page-cp-airbnb

---
+++ src/data.js
@@ -0,0 +1,41 @@
+export default [
+    {
+        id: 1,
+        title: "Life lessons with Katie Zaferes",
+        description: "desc",
+        price: 136,
+        coverImg: "Katie.png",
+        stats: {
+            rating: 5.0,
+            reviewCount: 6,
+        },
+        location: "Online",
+        openSpots: 0,
+    },
+    {
+        id: 2,
+        title: "Learn wedding photography",
+        description: "desc",
+        price: 125,
+        coverImg: "wedding.png",
+        stats: {
+            rating: 5.0,
+            reviewCount: 30,
+        },
+        location: "Online",
+        openSpots: 27,
+    },
+    {
+        id: 3,
+        title: "Group Mountain Biking",
+        description: "desc",
+        price: 50,
+        coverImg: "biking.png",
+        stats: {
+            rating: 4.8,
+            reviewCount: 2,
+        },
+        location: "Online",
+        openSpots: 0,
+    },
+]
